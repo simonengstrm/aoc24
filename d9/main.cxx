dcd299070b8c86afe8fdd4a6334f842fab972ad3
simonengstrm <simon_0_0_@live.se>
2024-12-10 15:04:32 +0100

    d9

--- d9/main.cxx
+++ d9/main.cxx
@@ -4,6 +4,74 @@
 
 using std::string, std::cout, std::endl;
 
+struct Block {
+    int id; // -1 for free
+    int size;
+};
+
+int part2(string input) {
+    std::vector<Block> disk;
+    int id = 0;
+    for (int i = 0; i < input.size(); i++) {
+        if (i % 2 == 0) {
+            // Put input[i] amount of id into disk
+            disk.push_back({id, input[i] - '0'});
+            id++;
+        } else {
+            disk.push_back({-1, input[i] - '0'});
+        }
+    }
+
+    for (int hi = disk.size()-1; hi >= 0; hi--) {
+        // Try to move disk[hi] to a free block
+        if (disk[hi].id == -1) {
+            continue;
+        }
+
+        // Try to find a free block large enought to move disk[hi] to
+        int lo = 0;
+        while (lo < hi) {
+            if (disk[lo].id != -1) {
+                lo++;
+                continue;
+            } else if (disk[hi].size <= disk[lo].size) {
+                break;
+            }
+            lo++;
+        }
+
+        if (lo == hi) {
+            continue;
+        }
+
+        int diff = disk[lo].size - disk[hi].size;
+        disk[lo].id = disk[hi].id;
+        disk[lo].size = disk[hi].size;
+        disk[hi].id = -1;
+        if (diff > 0) {
+            disk.insert(disk.begin() + lo + 1, {-1, diff});
+        }
+
+    }
+
+    long score = 0;
+    long i = 0;
+    for (auto [id, size] : disk) {
+        if (id == -1) {
+            i += size;
+            continue;
+        }
+        for (int j = 0; j < size; j++) {
+            score += id * i;
+            i++;
+        }
+    }
+
+    cout << "Score: " << score << endl;
+
+    return 0;
+}
+
 int part1(string input) {
     std::vector<int> disk; // -1 empty
     int id = 0;
@@ -40,8 +108,8 @@ int part1(string input) {
     }
 
     // Calculate score
-    int score = 0;
-    int i = 0;
+    long score = 0;
+    long i = 0;
     while (disk[i] != -1) {
         score += disk[i] * i;
         i++;
@@ -56,7 +124,8 @@ int main() {
     std::ifstream file("../d9/input.txt");
     string input;
     getline(file, input);
-    cout << "Part 1: " << part1(input) << endl;
+    part1(input);
+    part2(input);
     return 0;
 }
 
